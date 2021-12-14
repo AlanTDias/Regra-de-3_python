30824b87378b4b6eeb6cda36e69503890ccf9fa9
Alan Dias <alan_bmz@hotmail.com>
2021-12-14 13:07:14 -0300

    regra 3

---
+++ regra3.py
@@ -0,0 +1,13 @@
+n1 = int (input("Vamos precisar do Primeiro Numero "))
+n2 = int (input("Vamos precisar do Segundo Numero "))
+n3 = int (input("Vamos precisar do Terceiro Numero "))
+n4 = 1*100
+x = n3 * n2
+y = n1 * n4
+a = x/y
+
+
+print ('resultado: {}' .format(a))
+
+
+
